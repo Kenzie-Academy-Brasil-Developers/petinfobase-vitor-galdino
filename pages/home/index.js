807e6c9c67c87e3a9f73b48d598f7b1d05f20cb0
Vitor Galdino <httpvittor@gmail.com>
2022-10-21 20:33:33 -0300

    fix: if user not have permission, refresh page

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,9 @@
 import { RenderHomePage } from "../../scripts/renders.js";
 import { Permission } from "../../scripts/permissions.js";
 
+const refreshPage = () => !localStorage.getItem("userToken") ? location.reload() : false;
+refreshPage();
+
 Permission.userDontHaveToken();
 
 RenderHomePage.headerProfile();
--- pages/register/index.js
+++ pages/register/index.js
@@ -2,6 +2,9 @@ import { Api } from "../../scripts/apiRequests.js";
 import { Form } from "../../scripts/form.js";
 import { Permission } from "../../scripts/permissions.js";
 
+const refreshPage = () => localStorage.getItem("userToken") ? location.reload() : false;
+refreshPage();
+
 Permission.userHasToken();
 
 Form.checkInputs(".default-input", ".register-access");
