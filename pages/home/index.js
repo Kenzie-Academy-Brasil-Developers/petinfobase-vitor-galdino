2c610e68cbb6fee64cebee081bc20e0374a23f6a
Vitor Galdino <httpvittor@gmail.com>
2022-10-23 13:54:19 -0300

    test: refresh page function load at html

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,8 +1,8 @@
 import { RenderHomePage } from "../../scripts/renders.js";
 import { Permission } from "../../scripts/permissions.js";
 
-const refreshPage = () => !localStorage.getItem("userToken") ? location.reload() : false;
-refreshPage();
+// const refreshPage = () => !localStorage.getItem("userToken") ? location.reload() : false;
+// refreshPage();
 
 Permission.userDontHaveToken();
 
--- pages/register/index.js
+++ pages/register/index.js
@@ -2,8 +2,8 @@ import { Api } from "../../scripts/apiRequests.js";
 import { Form } from "../../scripts/form.js";
 import { Permission } from "../../scripts/permissions.js";
 
-const refreshPage = () => localStorage.getItem("userToken") ? location.reload() : false;
-refreshPage();
+// const refreshPage = () => localStorage.getItem("userToken") ? location.reload() : false;
+// refreshPage();
 
 Permission.userHasToken();
 
