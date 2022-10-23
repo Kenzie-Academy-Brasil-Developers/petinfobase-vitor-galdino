5437702732ea6f95fd6ab520b9e6fd90504605f1
Vitor Galdino <httpvittor@gmail.com>
2022-10-23 15:41:54 -0300

    fix

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,5 +2,7 @@ import { Api } from "../../scripts/apiRequests.js";
 import { Form } from "../../scripts/form.js";
 import { Permission } from "../../scripts/permissions.js";
 
+Permission.userHasToken();
+
 Form.checkInputs(".default-input", ".login-access");
 Form.getFormValues(".login-access", Api.loginRequest, "login");
--- pages/register/index.js
+++ pages/register/index.js
@@ -2,5 +2,7 @@ import { Api } from "../../scripts/apiRequests.js";
 import { Form } from "../../scripts/form.js";
 import { Permission } from "../../scripts/permissions.js";
 
+Permission.userHasToken();
+
 Form.checkInputs(".default-input", ".register-access");
 Form.getFormValues(".register-access", Api.registerRequest, "register");
